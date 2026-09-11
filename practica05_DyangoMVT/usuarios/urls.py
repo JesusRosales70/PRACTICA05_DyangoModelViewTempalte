9247fdb1415d39c76d47ca9da7bccba7cc0860be
JesuRosales70 <23040166@itdurango.edu.mx>
2026-09-10 20:10:52 -0600

    All update

--- practica05_DyangoMVT/usuarios/urls.py
+++ practica05_DyangoMVT/usuarios/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.inicio, name="inicio"),
+    path("login/", views.iniciar_sesion, name="login"),
 ]
--- practica05_DyangoMVT/usuarios/views.py
+++ practica05_DyangoMVT/usuarios/views.py
@@ -1,11 +1,39 @@
-from django.shortcuts import render
-from .models import Rol
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
+# ============================================================
+# VISTA: INICIO
+# ============================================================
 def inicio(request):
-    roles = Rol.objects.all()
+    rol = None
+    if request.user.is_authenticated:
+        try:
+            rol = request.user.perfilusuario.rol.nombre
+        except:
+            rol = "Sin rol asignado"
+            
     contexto = {
         "titulo": "Control de Usuarios",
         "mensaje": "Bienvenido a la Práctica 05 con Django",
-        "roles": roles
+        "rol": rol
     }
     return render(request, "usuarios/inicio.html", contexto)
+
+# ============================================================
+# VISTA: INICIAR SESIÓN
+# ============================================================
+def iniciar_sesion(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        
+        usuario = authenticate(request, username=username, password=password)
+        
+        if usuario is not None:
+            login(request, usuario)
+            return redirect("inicio")
+            
+        contexto = {"error": "Usuario o contraseña incorrectos"}
+        return render(request, "usuarios/login.html", contexto)
+        
+    return render(request, "usuarios/login.html")
