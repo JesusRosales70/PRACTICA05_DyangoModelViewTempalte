90f2cd7bed829a28e8ba9e6a9b3b06b41d72e6b9
JesuRosales70 <23040166@itdurango.edu.mx>
2026-09-10 20:56:55 -0600

    Install whitenoise

--- practica05_DyangoMVT/practica05_DyangoMVT/settings.py
+++ practica05_DyangoMVT/practica05_DyangoMVT/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # <-- Añadir esta línea
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Email
